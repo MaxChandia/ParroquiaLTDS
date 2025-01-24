8f8fb7b218b8ec89b03f978a98b162effd32a228
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-01-24 18:58:51 -0300

    cambios

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -3,10 +3,8 @@ import { Editor, EditorState } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 const MyEditor = () => {
-  // Crea un estado para el editor con un valor inicial vacío
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-  // Función para manejar los cambios en el editor
   const onEditorStateChange = (newState) => {
     setEditorState(newState);
   };
@@ -16,7 +14,7 @@ const MyEditor = () => {
       <h2>Editor de Draft.js</h2>
       <Editor 
         editorState={editorState}
-        onChange={onEditorStateChange} // Actualiza el estado del editor cuando el contenido cambia
+        onChange={onEditorStateChange}
       />
     </div>
   );
