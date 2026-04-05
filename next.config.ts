fd31b5509f185af56c2bc418589759fb53c96aec
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-04-05 19:56:24 -0400

    chore: cambio en paquetes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
   reactStrictMode: true,
-  experimental: {
-    reactRoot: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagenes-parroquia.s3.us-east-1.amazonaws.com',
+        port: '',
+        pathname: '/**', 
+      },
+    ],
   },
 };
 
--- src/cloudinary/config.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "dqp4mnozy", 
-  api_key: "341968887157697",     
-  api_secret: "BObg0rObw-YQ8TqlvNcI1ruEwuY", 
-});
-
-export default cloudinary;
