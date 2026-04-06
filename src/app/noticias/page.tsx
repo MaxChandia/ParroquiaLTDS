268b5813d4b83ade79fd24fbad8e1a4565f1e771
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-04-05 20:57:49 -0400

    refactor: img a Image

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Navbar from "src/components/navbar.jsx";
 import Footer from "src/components/footer";
-import NewsContainer from "src/components/newsContainer/newsContainer.jsx";
 import '../../styles/noticias.css';
 import NewsGrid from "@/components/newsGrid/newsGrid";
 
